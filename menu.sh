d5f95e2c6b57510121b65ae30dcb05d92d52d50e
marcui13 <soyyocorta@gmail.com>
2022-07-28 21:38:33 -0300

    modif menu

--- menu.sh
+++ menu.sh
@@ -4,17 +4,12 @@ echo ---------------------------------------------------------------------------
 echo ------------------------------------MENU-------------------------------------
 echo -----------------------------------------------------------------------------
 
-<<<<<<< HEAD
 
 PS3="Please select the script to run: "
 SCRIPTS=("statsWords" "statsUsageWords" "findNames" "statsSentences" "blankLinesCounter" "caseConverter" "substringReplace" "blockSelection" "palindromeDetection")
 
-=======
-PS3="Please select the script to run: "
-SCRIPTS=("statsWords" "statsUsageWords" "findNames" "statsSentences" "blankLinesCounter" "caseConverter" "substringReplace" "blockSelection" "palindromeDetection")
 
 
->>>>>>> palindromeDetection
 select scr in  "${SCRIPTS[@]}" Quit
 do
     case  $scr in
@@ -24,7 +19,6 @@ do
            echo "$scr - is statsUsageWords";;
         "findNames")
            echo "$scr - is findNames";;
-<<<<<<< HEAD
    	"statsSentences")
 	   echo "$scr - is statsSentences";;
 	"blankLinesCounter")
@@ -44,33 +38,10 @@ do
            ;;
 
    	"Quit")
-=======
-        "statsSentences")
-           echo "$scr - is statsSentences";;
-        "blankLinesCounter")
-           ./blankLinesCounter.sh lorem.txt
-	   ;;
-        "caseConverter")
-           echo "$scr - is caseConverter";;
-        "substringReplace")
-           echo "$scr - is substringReplace";;
-        "blockSelection")
-           echo "$scr - is blockSelection";;
-        "palindromeDetection")
-           PAL="ingrese la palabra a analizar: "
-	   echo $PAL
-	   read PAL
-	   ./palindromeDetection.sh $PAL
-	   ;;
-        "Quit")
->>>>>>> palindromeDetection
            echo "We're done"
            break;;
         *)
            echo "Ooops";;
     esac
 done
-<<<<<<< HEAD
-=======
 
->>>>>>> palindromeDetection

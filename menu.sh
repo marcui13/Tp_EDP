45c819ddf358465db65e0ffad6804c6861c96390
marcui13 <soyyocorta@gmail.com>
2022-07-30 18:57:25 -0300

    modif menu

--- menu.sh
+++ menu.sh
@@ -4,3 +4,42 @@ echo ---------------------------------------------------------------------------
 echo ------------------------------------MENU-------------------------------------
 echo -----------------------------------------------------------------------------
 
+PS3="Please select the script to run: "
+SCRIPTS=("statsWords" "statsUsageWords" "findNames" "statsSentences" "blankLinesCounter" "caseConverter" "substringReplace" "blockSelection" "palindromeDetection")
+
+
+
+select scr in  "${SCRIPTS[@]}" Quit
+do
+    case  $scr in
+        "statsWords")
+	   ./statsWords.sh $(cat lorem.txt)
+	   ;;
+        "statsUsageWords")
+           echo "$scr - is statsUsageWords";;
+        "findNames")
+           echo "$scr - is findNames";;
+   	"statsSentences")
+	   echo "$scr - is statsSentences";;
+	"blankLinesCounter")
+           ./blankLinesCounter.sh lorem.txt
+	   ;;
+	"caseConverter")
+	   echo "$scr - is caseConverter";;
+	"substringReplace")
+	   echo "$scr - is substringReplace";;
+	"blockSelection")
+           echo "$scr - is blockSelection";;
+	"palindromeDetection")
+  	   PAL="ingrese la palabra a analizar: "
+           echo $PAL
+           read PAL
+           ./palindromeDetection.sh $PAL
+           ;;
+        "Quit")
+           echo "We're done"
+           break;;
+        *)
+           echo "Ooops";;
+    esac
+done

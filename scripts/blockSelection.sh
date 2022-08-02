74859c6398773cb5183e0f0dee3e22ba0fbf0ff2
Ariel Santillan <ariel.rafael.santillan@gmail.com>
2022-08-02 11:43:54 -0300

    modificacion script blockSelection

--- scripts/blockSelection.sh
+++ scripts/blockSelection.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-IN=$(sed '/^$/d' prueba.txt > prueba2.txt)
-INPUT=$(cat prueba2.txt)
-PUNTO='([.]$)' #-----------------------termina en punto
+IN=$(sed '/^$/d' lorem.txt > lorem2.txt) #---------elimina las lineas en blanco entre parrafos y guarda el contenido en un nuevo archivo.
+INPUT=$(cat lorem2.txt) #-------------guarda en la variable el contenido del archivo.
+PUNTO='([.]$)' #-----------------------REGEX: termina en punto
 
-oraciones=("Oracion numero 0!")  #-------lista con oraciones 
+oraciones=("")  #-------lista con oraciones 
 
 #-------construccion de lista con oraciones separadas e identificadas.
 
 for palabra in $INPUT;do
 	declare -a acumulador
-	if ! [[ $palabra =~ $PUNTO ]];then
+	if ! [[ $palabra =~ $PUNTO ]];then #si la palabra no termina en punto la agrega al acumulador.
 		acumulador+="$palabra "
-	else
+	else				#si la palabra termina en punto la agrega a la oracion,agrega la oracion a lista de oraciones y reinicia el acumulador.
 		acumulador+="$palabra "
 		oraciones+=("${acumulador[@]}")
 		acumulador=""
@@ -19,38 +19,38 @@ for palabra in $INPUT;do
 done
 
 #----------------------------------------------------------------------
-replace=$( tr "\n" "@" < prueba.txt ) #identifico cada salto de linea. 
-formating=("$(echo ${replace//@/ @ })")
+replace=$( tr "\n" "@" < lorem2.txt ) #identifico cada salto de linea con '@' 
+formating=("$(echo ${replace//@/ @ })") #reemplaza '@' por ' @ '
 
 #--construccion de lista de oraciones separadas por parrafos-----------
 
-parrafos=""
-IDENTIFICADOR='([@])'
+parrafos=""  #---------------lista de parrafos
+IDENTIFICADOR='([@])' #-------cada parrafo termina con un '@'
 
 for palabra in $formating;do
 	declare -a acumulador
-	if ! [[ $palabra =~ $IDENTIFICADOR ]];then
+	if ! [[ $palabra =~ $IDENTIFICADOR ]];then       #si la palabra no termina con '@'la agraga al acumulador.
 		acumulador+="$palabra "
 	else
-		parrafos+=("${acumulador[@]}")
+		parrafos+=("${acumulador[@]}")		#cuando se topa con un '@' agrega la lista acumulada al array 'parrafos'
 		acumulador=""
 	fi
 done
 #------------------------------------------------------------------------
 
-function select_oracion(){
-	read -p "Que numero de oracion desea consultar? " valor
+function select_oracion(){  #------------------------------------pide eleccion de numero(indice) de oracion consultar y la muestra por pantalla
+	read -p "Que numero de oracion desea consultar? " valor   
 	echo "${oraciones[$valor]}"
 }
 
-function select_parrafo(){
+function select_parrafo(){ #-------------------------------------pide eleccion de numero de parrafo a consultar y lo muestra por pantalla.
 	read -p "Que numero de parrafo desea consultar? " valor
         echo "${parrafos[$valor]}"	
 }
 
-echo "Que deseas consultar? "
+PS3="Que deseas consultar? "
 
-select funcion in oracion parrafo
+select funcion in oracion parrafo #------------select: ejecuta funciones dependiendo la eleccion del usuario.
 do
 	case $funcion in
 		oracion)
@@ -60,7 +60,7 @@ do
 			select_parrafo
 		;;
 		*)
-			echo "opcion invalida: $REPLY"
+			echo "opcion invalida: $REPLY" # si se ingresa una opcion no valida(fuera del rango)
 		;;
 	esac
 	break
